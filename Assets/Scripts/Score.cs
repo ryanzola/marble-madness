b6427fe75ecf91cbecc67122f79ae909586de30a
ryanzola <ryanzola@me.com>
2022-10-09 01:37:33 -0400

    💯 update score calculator

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
+using System.Collections;
 using TMPro;
 
 public class Score : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI m_scoreText;
-    public int ScoreValue { get; private set; }
+    [SerializeField] float m_levelMultiplier;
+    RollingMovement m_rollingMovement;
+    public float ScoreValue { get; private set; }
+    public Vector3 startPos;
+    public Vector3 currentPos;
+    float posDelta = 0f;
+    int current = 0;
+
+    private void Start() {
+        startPos = transform.position;
+        currentPos = transform.position;
+        m_rollingMovement = GetComponent<RollingMovement>();
+    }
 
     private void FixedUpdate() {
         // Every fixed update, update the score string
-        m_scoreText.text = ScoreValue.ToString();
+        StartCoroutine(CountUpToTarget());
     }
 
     // Start is called before the first frame update
     public void ResetScore()
     {
-        ScoreValue = 0;
+        ScoreValue = 0f;
+        // Distance = 0f;
     }
 
-    // Update is called once per frame
-    public void IncrementScore()
+
+
+    private void CalculateDistance() {
+        currentPos = transform.position;
+
+        posDelta = Vector3.Distance(startPos, currentPos);
+        posDelta *= m_levelMultiplier;
+        // Debug.Log((int)posDelta);
+
+
+        // Distance += 0.01f;
+    }
+
+    IEnumerator CountUpToTarget()
     {
-        ScoreValue+=10;
+            CalculateDistance();
+            int delta = (int)posDelta;
+
+            if(posDelta > 1 && (int)posDelta % 10 == 0 && delta != current) {
+                // if((int)posDelta == current) yield return null;
+                
+                current = delta;
+                ScoreValue += 10f;
+                m_scoreText.text = (int)ScoreValue + "";
+            }
+
+            // ScoreValue += Time.deltaTime; // or whatever to get the speed you like
+            // ScoreValue = Mathf.Clamp(ScoreValue, 0f, 1000f);
+            // m_scoreText.text = (int)ScoreValue + "";
+            yield return null;
     }
 }
